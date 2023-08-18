6c59e07b3fc86cb3280b3840fffab23633d43d5c
Agustín Guerrero <agusguerrero345@gmail.com>
2023-08-18 14:50:51 -0300

    actualización login

--- js/login.js
+++ js/login.js
@@ -9,8 +9,26 @@ function submitForm(event){
         localStorage.setItem('usuario', usuario);
         window.location.href = "index.html";
     } else {
-        errorMessage = "usuario y/o contrasena requerido";
+        errorMessage = "usuario y contrasena requerido";
     }
 
     loginError.innerHTML = errorMessage;
 };
+
+//Mostrar contraseña;
+function mostrarPassword() {
+    var contrasena = document.getElementById("contrasena");
+   var show_eye = document.getElementById("show_eye");
+   var hide_eye = document.getElementById("hide_eye");
+
+ hide_eye.classList.remove("d-none");
+ if (contrasena.type === "password") {
+   contrasena.type = "text";
+   show_eye.style.display = "none";
+   hide_eye.style.display = "block";
+ } else {
+   contrasena.type = "password";
+   show_eye.style.display = "block";
+   hide_eye.style.display = "none";
+ }
+}
